8c0a0cdb8f89eda9be2a8d88eaf0ef8bd625d044
Matt Hinchliffe <matt.hinchliffe@ft.com>
2021-06-02 12:19:46 +0100

    Ensure test database collection options match the ones used in the main package

--- test/helpers/createTestDB.js
+++ test/helpers/createTestDB.js
@@ -7,8 +7,14 @@ const imagesData = require('../fixtures/imagesData.json');
 module.exports = function createTestDB() {
 	const db = new Loki('test.db');
 
-	db.addCollection('pages', { clone: true }).insert(pagesData);
-	db.addCollection('images', { clone: true }).insert(imagesData);
+	const collectionOptions = {
+		unique: ['id', 'url'],
+		disableMeta: true,
+		clone: true,
+	};
+
+	db.addCollection('pages', collectionOptions).insert(pagesData);
+	db.addCollection('images', collectionOptions).insert(imagesData);
 
 	return db;
 };
